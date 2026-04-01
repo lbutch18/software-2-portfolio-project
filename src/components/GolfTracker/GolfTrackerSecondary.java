c9f9f40e4b6c305741c1b71350047192c7c8a461
Luke Butcher <butcher_l@icloud.com>
2026-03-31 22:39:49 -0400

    Implement secondary methods

---
+++ src/components/GolfTracker/GolfTrackerSecondary.java
@@ -0,0 +1,183 @@
+package components.GolfTracker;
+
+import java.util.Arrays;
+
+import components.GolfTracker.GolfTracker1.Course;
+import components.GolfTracker.GolfTracker1.Round;
+import components.sequence.Sequence;
+
+public abstract class GolfTrackerSecondary implements GolfTracker {
+
+    /**
+     * Helper for handicap calculation. Averages lowest x diffs of last n
+     * rounds.
+     *
+     * @param x
+     *            number of lowest diffs to find
+     * @param n
+     *            number of diffs to find lowest diffs from
+     * @return the lowest x of n diffs
+     */
+    private double averageLowestXOfNDiffs(int x, int n) {
+        double[] diffs = new double[n];
+
+        int count = 0;
+        Sequence<Round> allRounds = this.getAllRounds();
+        for (int i = allRounds.length() - 1; i >= allRounds.length() - n; i--) {
+            diffs[count] = allRounds.entry(i).getDiff();
+            count++;
+        }
+
+        Arrays.sort(diffs);
+        double sum = 0;
+        for (int i = 0; i < x; i++) {
+            sum += diffs[i];
+        }
+
+        double handicap = sum / x;
+        return Math.round(handicap * 10.0) / 10.0;
+    }
+
+    /**
+     * Simple USGA-based handicap calculation for the rounds in this.
+     *
+     * @return the handicap given the rounds in this
+     * @requires number of rounds in this >= 3
+     * @ensures calculateHandicap = handicap rounded to tenths place
+     */
+    @Override
+    public double calculateHandicap() {
+        double handicap = 0;
+        if (this.numRounds() >= 20) {
+            handicap = this.averageLowestXOfNDiffs(8, 20);
+        } else if (this.numRounds() == 19) {
+            handicap = this.averageLowestXOfNDiffs(7, this.numRounds());
+        } else if (this.numRounds() >= 18) {
+            handicap = this.averageLowestXOfNDiffs(6, this.numRounds());
+        } else if (this.numRounds() >= 15) {
+            handicap = this.averageLowestXOfNDiffs(5, this.numRounds());
+        } else if (this.numRounds() >= 12) {
+            handicap = this.averageLowestXOfNDiffs(4, this.numRounds());
+        } else if (this.numRounds() >= 9) {
+            handicap = this.averageLowestXOfNDiffs(3, this.numRounds());
+        } else if (this.numRounds() >= 7) {
+            handicap = this.averageLowestXOfNDiffs(2, this.numRounds());
+        } else if (this.numRounds() == 6) {
+            handicap = this.averageLowestXOfNDiffs(2, this.numRounds()) - 1.0;
+        } else if (this.numRounds() == 5) {
+            handicap = this.averageLowestXOfNDiffs(1, this.numRounds());
+        } else if (this.numRounds() == 4) {
+            handicap = this.averageLowestXOfNDiffs(1, this.numRounds()) - 1.0;
+        } else if (this.numRounds() == 3) {
+            handicap = this.averageLowestXOfNDiffs(1, this.numRounds()) - 2.0;
+        }
+
+        return handicap;
+    }
+
+    /**
+     * Return the best round by diff in the tracker.
+     *
+     * @return the round with the lowest differential
+     * @requires number of rounds in this > 0
+     * @ensures bestRound = round with least differential in this
+     * @aliases bestRound
+     */
+    @Override
+    public Round bestRound() {
+        Sequence<Round> allRounds = this.getAllRounds();
+        Round best = allRounds.entry(0);
+        for (int i = 1; i < allRounds.length(); i++) {
+            if (allRounds.entry(i).getDiff() < best.getDiff()) {
+                best = allRounds.entry(i);
+            }
+        }
+
+        return best;
+    }
+
+    /**
+     * Return the average differential in the tracker.
+     *
+     * @return the average differential of all rounds in this
+     * @requires number of rounds in this > 0
+     * @ensures averageDiff = mean differential of all Rounds in this
+     */
+    @Override
+    public double averageDiff() {
+        Sequence<Round> allRounds = this.getAllRounds();
+        double average = 0;
+        for (int i = 0; i < allRounds.length(); i++) {
+            average += allRounds.entry(i).getDiff();
+        }
+
+        return average;
+    }
+
+    /**
+     * Return the average score for rounds with given holes played in the
+     * tracker.
+     *
+     * @param holesPlayed
+     *            the number of holes played for which the average score is to
+     *            be calculated
+     * @return the average score of all rounds in this
+     * @requires number of rounds in this > 0
+     * @ensures averageScore = mean score of all Rounds in this
+     */
+    @Override
+    public double averageScore(int holesPlayed) {
+        Sequence<Round> allRounds = this.getAllRounds();
+        double average = 0;
+
+        for (int i = 0; i < allRounds.length(); i++) {
+            if (allRounds.entry(i).getHolesPlayed() == holesPlayed) {
+                average += allRounds.entry(i).getScore();
+            }
+        }
+
+        return average;
+    }
+
+    /**
+     * Returns the Sequence of all rounds played at the given course.
+     *
+     * @param course
+     *            the course to get the rounds at
+     * @return Sequence of Rounds played the given Course
+     * @requires number of rounds played at course > 0
+     * @ensures roundsAtCourse = Sequence of all Rounds played at course
+     */
+    @Override
+    public Sequence<Round> roundsAtCourse(Course course) {
+        Sequence<Round> rounds = this.getAllRounds();
+
+        for (int i = 0; i < rounds.length(); i++) {
+            if (!rounds.entry(i).getCourse().equals(course)) {
+                rounds.remove(i);
+            }
+        }
+
+        return rounds;
+    }
+
+    /**
+     * Intended to allow for editing of rounds. Replaces a round in the tracker
+     * with the new round.
+     *
+     * @param oldRound
+     *            the old round to be removed
+     * @param newRound
+     *            the new round to be added
+     * @requires oldRound is a Round in this
+     * @ensures oldRound is not in this and copy of newRound is in this
+     * @updates this
+     */
+    @Override
+    public void replaceRound(Round oldRound, Round newRound) {
+        this.deleteRound(oldRound);
+        this.addRound(newRound.getScore(), newRound.getHolesPlayed(),
+                newRound.getDate()[0], newRound.getDate()[1],
+                newRound.getDate()[2], newRound.getID(), newRound.getCourse());
+    }
+}
