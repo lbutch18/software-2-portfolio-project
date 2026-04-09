cbdab0d945b13d5cdf8e8fb1ea29fd7fe603b582
Luke Butcher <butcher_l@icloud.com>
2026-04-09 13:11:42 -0400

    Implement common methods

--- src/components/GolfTracker/GolfTrackerSecondary.java
+++ src/components/GolfTracker/GolfTrackerSecondary.java
@@ -38,13 +38,6 @@ public abstract class GolfTrackerSecondary implements GolfTracker {
         return Math.round(handicap * 10.0) / 10.0;
     }
 
-    /**
-     * Simple USGA-based handicap calculation for the rounds in this.
-     *
-     * @return the handicap given the rounds in this
-     * @requires number of rounds in this >= 3
-     * @ensures calculateHandicap = handicap rounded to tenths place
-     */
     @Override
     public double calculateHandicap() {
         double handicap = 0;
@@ -75,14 +68,6 @@ public abstract class GolfTrackerSecondary implements GolfTracker {
         return handicap;
     }
 
-    /**
-     * Return the best round by diff in the tracker.
-     *
-     * @return the round with the lowest differential
-     * @requires number of rounds in this > 0
-     * @ensures bestRound = round with least differential in this
-     * @aliases bestRound
-     */
     @Override
     public Round bestRound() {
         Sequence<Round> allRounds = this.getAllRounds();
@@ -96,13 +81,6 @@ public abstract class GolfTrackerSecondary implements GolfTracker {
         return best;
     }
 
-    /**
-     * Return the average differential in the tracker.
-     *
-     * @return the average differential of all rounds in this
-     * @requires number of rounds in this > 0
-     * @ensures averageDiff = mean differential of all Rounds in this
-     */
     @Override
     public double averageDiff() {
         Sequence<Round> allRounds = this.getAllRounds();
@@ -114,17 +92,6 @@ public abstract class GolfTrackerSecondary implements GolfTracker {
         return average;
     }
 
-    /**
-     * Return the average score for rounds with given holes played in the
-     * tracker.
-     *
-     * @param holesPlayed
-     *            the number of holes played for which the average score is to
-     *            be calculated
-     * @return the average score of all rounds in this
-     * @requires number of rounds in this > 0
-     * @ensures averageScore = mean score of all Rounds in this
-     */
     @Override
     public double averageScore(int holesPlayed) {
         Sequence<Round> allRounds = this.getAllRounds();
@@ -139,15 +106,6 @@ public abstract class GolfTrackerSecondary implements GolfTracker {
         return average;
     }
 
-    /**
-     * Returns the Sequence of all rounds played at the given course.
-     *
-     * @param course
-     *            the course to get the rounds at
-     * @return Sequence of Rounds played the given Course
-     * @requires number of rounds played at course > 0
-     * @ensures roundsAtCourse = Sequence of all Rounds played at course
-     */
     @Override
     public Sequence<Round> roundsAtCourse(Course course) {
         Sequence<Round> rounds = this.getAllRounds();
@@ -161,23 +119,35 @@ public abstract class GolfTrackerSecondary implements GolfTracker {
         return rounds;
     }
 
-    /**
-     * Intended to allow for editing of rounds. Replaces a round in the tracker
-     * with the new round.
-     *
-     * @param oldRound
-     *            the old round to be removed
-     * @param newRound
-     *            the new round to be added
-     * @requires oldRound is a Round in this
-     * @ensures oldRound is not in this and copy of newRound is in this
-     * @updates this
-     */
     @Override
     public void replaceRound(Round oldRound, Round newRound) {
         this.deleteRound(oldRound);
         this.addRound(newRound.getScore(), newRound.getHolesPlayed(),
                 newRound.getDate()[0], newRound.getDate()[1],
                 newRound.getDate()[2], newRound.getID(), newRound.getCourse());
+    }
+
+    @Override
+    public String toString() {
+        Sequence<Round> allRounds = this.getAllRounds();
+        return allRounds.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        Sequence<Round> allRounds = this.getAllRounds();
+        return allRounds.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object compared) {
+        Sequence<Round> thisTrackerRounds = this.getAllRounds();
+        if (compared instanceof GolfTracker) {
+            Sequence<Round> comparedTrackerRounds = ((GolfTracker) compared)
+                    .getAllRounds();
+            return thisTrackerRounds.equals(comparedTrackerRounds);
+        } else {
+            return false;
+        }
     }
 }
